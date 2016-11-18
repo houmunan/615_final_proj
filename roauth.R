f1d625cfddb3fa20417cddf6b21880e0c2ce29c3
houmunan <449020073@qq.com>
2016-11-18 15:59:51 -0500

    Oh

---
+++ roauth.R
@@ -0,0 +1,16 @@
+install.packages("ROAuth")
+
+
+
+library(ROAuth)
+requestURL <- "https://api.twitter.com/oauth/request_token"
+accessURL <- "https://api.twitter.com/oauth/access_token"
+authURL <- "https://api.twitter.com/oauth/authorize"
+consumerKey <- "gGukhMfab88IYCTNYwOE8beUi"
+consumerSecret <- "qdDP6b2Z4zlUf5X5863g4qMbZz92JWQNHqstjrkEWOp0GZN71N"
+my_oauth <- OAuthFactory$new(consumerKey = consumerKey, consumerSecret = consumerSecret, 
+                             requestURL = requestURL, accessURL = accessURL, authURL = authURL)
+my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
+save(my_oauth, file = "my_oauth.Rdata")
+
+4647567
